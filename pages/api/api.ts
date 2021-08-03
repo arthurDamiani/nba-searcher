829a12f648b56f80a2fd52a9935b625df2ca62e8
arthurDamiani <arthur.damiani@gmail.com>
2021-08-02 23:07:14 -0300

    test

--- pages/api/api.ts
+++ pages/api/api.ts
@@ -3,7 +3,7 @@ import axios from 'axios'
 const api = axios.create({
     baseURL: 'https://free-nba.p.rapidapi.com/',
     headers: {
-        'X-RapidAPI-Key': process.env.NEXT_PUBLIC_API_KEY,
+        'X-RapidAPI-Key': process.env.API_KEY,
         'X-RapidAPI-Host': 'free-nba.p.rapidapi.com'
     }
 })
